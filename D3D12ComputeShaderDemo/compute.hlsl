3b4a5a588ff0ed35cabd5f573cb143ac14a35f3f
Zenny Chen <zenny_chen@sina.com>
2019-10-08 04:13:26 +0800

    Update compute.hlsl

--- D3D12ComputeShaderDemo/compute.hlsl
+++ D3D12ComputeShaderDemo/compute.hlsl
@@ -1,6 +1,6 @@
 
-StructuredBuffer<int> srcBuffer: register(t0);	    // SRV
-RWStructuredBuffer<int> dstBuffer: register(u0);	// UAV
+StructuredBuffer<int> srcBuffer: register(t0);      // SRV
+RWStructuredBuffer<int> dstBuffer: register(u0);    // UAV
 
 [numthreads(1024, 1, 1)]
 void CSMain(uint3 groupID : SV_GroupID, uint3 tid : SV_DispatchThreadID, uint3 localTID : SV_GroupThreadID, uint groupIndex : SV_GroupIndex)
